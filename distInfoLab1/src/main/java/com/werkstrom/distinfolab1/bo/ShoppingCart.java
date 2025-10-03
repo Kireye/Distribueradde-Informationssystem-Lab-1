3405e56d9920e7d9e216fdb4e099d557bea31cf6
Amir Leo Alshammaa <amir.alshamaa@gmail.com>
2025-10-03 19:54:45 +0200

    added methods in ShoppingCart

--- distInfoLab1/src/main/java/com/werkstrom/distinfolab1/bo/ShoppingCart.java
+++ distInfoLab1/src/main/java/com/werkstrom/distinfolab1/bo/ShoppingCart.java
@@ -19,5 +19,32 @@ public class ShoppingCart {
         this.items = new ArrayList<>(items);
     }
 
+    public int getOwnerId() {
+        return ownerId;
+    }
 
+    public List<Item> getItems() {
+        return new ArrayList<>(items);
+    }
+
+    public float getTotalPrice(List<Item> items) {
+        float totalPrice = 0;
+        for (int i = 0; i < items.size(); i++) {
+            totalPrice += items[i].getPrice;
+        }
+    }
+
+    public void clearItems() {
+        for (int i = 0; i < items.size(); i++) {
+            items.remove(i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "ownerId=" + ownerId +
+                ", items=" + items +
+                '}';
+    }
 }
