77bd667eb939dddca8d5e12a9434ec9cb51bc1e0
Erik Werkström <erik@werkstrom.com>
2025-10-03 18:16:45 +0200

    update MySqlConnectionManager

--- distInfoLab1/src/main/java/com/werkstrom/distinfolab1/db/MySqlConnectionManager.java
+++ distInfoLab1/src/main/java/com/werkstrom/distinfolab1/db/MySqlConnectionManager.java
@@ -1,10 +1,9 @@
 package com.werkstrom.distinfolab1.db;
 
 import com.werkstrom.distinfolab1.db.exceptions.ConnectionException;
+import com.werkstrom.distinfolab1.db.exceptions.QueryException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySqlConnectionManager {
     private static MySqlConnectionManager instance = null;
@@ -36,15 +35,17 @@ public class MySqlConnectionManager {
                 instance = null;
             }
             catch (SQLException e) {
-                throw new ConnectionException(e.getMessage());
+                throw new ConnectionException("Closing connection failed: " + e.getMessage());
             }
     }
 
+    /* TODO: Delete?
     public static Connection getConnection() throws ConnectionException {
         if (!isConnected())
             throw new ConnectionException("No connection initialized.");
         return instance.connection;
     }
+     */
 
     public static boolean isConnected() {
         if (instance == null || instance.connection == null) return false;
@@ -52,7 +53,25 @@ public class MySqlConnectionManager {
             return !instance.connection.isClosed();
         }
         catch (SQLException e) {
-            throw new ConnectionException(e.getMessage());
+            throw new ConnectionException("Failure while getting connection status: " + e.getMessage());
+        }
+    }
+
+    public static PreparedStatement createPreparedStatement(String query) throws QueryException {
+        try {
+            return instance.connection.prepareStatement(query);
+        }
+        catch (SQLException e) {
+            throw new QueryException("Failure while trying to prepare statement" + e.getMessage());
+        }
+    }
+
+    public static ResultSet executeQuery(PreparedStatement preparedStatement) throws QueryException {
+        try {
+            return preparedStatement.executeQuery();
+        }
+        catch (SQLException e) {
+            throw new QueryException("Failure while trying to execute query" + e.getMessage());
         }
     }
 }
