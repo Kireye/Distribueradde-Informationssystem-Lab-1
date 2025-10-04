3ce5eac0c726e49f23c9379288527b196057e464
Erik Werkström <erik@werkstrom.com>
2025-10-04 13:35:22 +0200

    create show_all_tables.sql

---
+++ sql/show_all_tables.sql
@@ -0,0 +1,15 @@
+USE online_store;
+
+SELECT * FROM User;
+
+SELECT * FROM Item;
+
+SELECT * FROM Item_category;
+
+SELECT * FROM Item_category_mapping;
+
+SELECT * FROM Shopping_cart;
+
+SELECT * FROM Customer_order;
+
+SELECT * FROM Order_item_mapping;
