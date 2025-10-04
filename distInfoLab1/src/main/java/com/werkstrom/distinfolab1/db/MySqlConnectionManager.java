64bbf54d91c367e5df8bcf4525a68a964fec9dc8
Erik Werkström <erik@werkstrom.com>
2025-10-04 11:56:30 +0200

    Update MySqlConnectionManager.java

--- distInfoLab1/src/main/java/com/werkstrom/distinfolab1/db/MySqlConnectionManager.java
+++ distInfoLab1/src/main/java/com/werkstrom/distinfolab1/db/MySqlConnectionManager.java
@@ -15,18 +15,16 @@ public class MySqlConnectionManager {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_store", username, password);
         }
         catch (Exception e) {
-            e.printStackTrace();
             throw new ConnectionException(e.getMessage());
         }
 
     }
 
-    public static Connection initializeConnection(String username, String password) throws ConnectionException {
+    public static void initializeConnection(String username, String password) throws ConnectionException {
         if (isConnected())
             throw new ConnectionException("Connection already initialized. To open a new connection close the active connection first.");
 
         instance = new MySqlConnectionManager(username, password);
-        return instance.connection;
     }
 
     public static void closeConnection() {
@@ -39,14 +37,6 @@ public class MySqlConnectionManager {
                 throw new ConnectionException("Closing connection failed: " + e.getMessage());
             }
     }
-
-    /* TODO: Delete?
-    public static Connection getConnection() throws ConnectionException {
-        if (!isConnected())
-            throw new ConnectionException("No connection initialized.");
-        return instance.connection;
-    }
-     */
 
     public static boolean isConnected() {
         if (instance == null || instance.connection == null) return false;
@@ -63,18 +53,8 @@ public class MySqlConnectionManager {
             return instance.connection.prepareStatement(query);
         }
         catch (SQLException e) {
-            throw new QueryException("Failure while trying to prepare statement" + e.getMessage());
+            throw new QueryException("Failure while trying to prepare statement: " + e.getMessage());
         }
     }
 
-    /* TODO: Delete?
-    public static ResultSet executeQuery(PreparedStatement preparedStatement) throws QueryException {
-        try {
-            return preparedStatement.executeQuery();
-        }
-        catch (SQLException e) {
-            throw new QueryException("Failure while trying to execute query" + e.getMessage());
-        }
-    }
-     */
 }
